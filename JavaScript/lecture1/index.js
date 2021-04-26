53727e19c9e7c038f038d05a11798497b284e6a0
Stefkoo98 <Simjanovski98@outlook.com>
2021-04-26 22:10:50 +0200

    basic stuff

---
+++ JavaScript/lecture1/index.js
@@ -0,0 +1,79 @@
+// var ime = "Stefan";
+// let a = 'a';
+// const b = 1;
+
+// console.log(ime);
+// console.log(5);
+
+
+// // podatochni tipovi:String, character, integer(cel broj), float, double, boolean(true/false)
+
+// let Ime = "Tamara"
+// console.log(Ime)
+// //  imeto na promenlivata smee da sodrzi broevi, _, $, megjutoa ne smee da zapocnuva na broj
+
+// let _123ab = "neshto";
+// console.log( _123ab);
+
+// +,-,/,*,%-modul, +=, -=, *=, /*, %=; ++, --;
+// >, <,<=, >=, ==, ===, -logicki operatori    !, !=, ||-ili (or), &&
+// (5>6)||(5>4); ()||()||()...
+// t||t ->t
+// t||f ->t
+// f||t ->t
+// f||f ->f
+
+// (a>5) && (b>6)
+// t&&t ->t
+// t&&f ->f
+// f&&t ->f
+// f&&f ->f
+
+// let broj1 = 10;
+// broj1 = broj1 + 10;
+
+// broj1+=10;
+
+// console.log(broj1+10);
+
+// console.log("5"==5); //sporeduva spored vrednost
+// console.log("5"===5); //sporeduva spored podatocen tip
+
+// let broj  = 10;
+// console.log(broj%3);
+
+// let n1 = "50";
+// let n2 = 50;
+// console.log(n1==n2 && n1===n2);
+// console.log(n1===n2 & n1++);
+// console.log(n1)
+
+// let broj = 10;
+// console.log(broj++);//ke go ispecati brojot pa ke go zgolemi;
+// console.log(++broj);
+
+// ako sobirame string so broj, rezultatot e string so isklucok na ++ ako stringot e broj;
+// let a = "10";
+// console.log(a+10);
+
+// let ime = "Stefan"
+// let prezime = "Simjanovski"
+// let imeIPrezime = ime+" "+prezime;
+
+// console.log (imeIPrezime+" "+27)
+
+// alert("Zdravo");
+// alert(imeIPrezime);
+
+
+// let ime = prompt("What's your name?");
+// console.log(ime);
+
+// let godini = prompt("Vnesi godini");
+// // godini = Number(godini);
+// godini = parseInt(godini);
+// console.log(godini);
+// console.log(parseInt(10.5));
+
+let flag = true;
+console.log(!flag);
