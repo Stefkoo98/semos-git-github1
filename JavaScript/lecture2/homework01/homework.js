f6ce272d0d137266fb0a136b8d6f674289013d6b
Stefkoo98 <Simjanovski98@outlook.com>
2021-05-05 18:05:57 +0200

    homeworkJS

---
+++ JavaScript/lecture2/homework01/homework.js
@@ -0,0 +1,84 @@
+// Pozitiven, Negativen broj 
+
+// let number = Number(prompt("Take a number"));
+
+// if(number > 0) {
+//     console.log("Positive number");
+// }
+// else if(number < 0) {
+//     console.log("Negative number");
+// }
+// else {
+//     console.log("Number zero");
+// }
+
+
+// Prestapna godina
+
+// function checkLeapYear(year) {
+
+//     if((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+//         console.log(year + ' is a leap year');
+//     } 
+//     else {
+//         console.log(year + ' is not a leap year');
+//     }
+// }
+// let year = prompt("Enter a year");
+// checkLeapYear(year);
+
+
+// Koordinanten sistem 
+
+// let x = Number(prompt("Vnesi broj za x"))
+// let y = Number(prompt("Vnesi broj za y"))
+
+// if(x > 0 && y > 0) {
+//     console.log("Prv kvadrant");
+// }
+// else if(x < 0 && y > 0) {
+//     console.log("Vtor kvadrant");
+// }
+// else if(x < 0 && y < 0) {
+//     console.log("Tret kvadrant");
+// }
+// else if(x > 0 && y < 0) {
+//     console.log("Cetvrt kvadrant");
+// }
+// else {
+//     console.log("Kvadranten pocetok");
+// }
+
+
+// Kalkulator
+
+let operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+let number1 = parseFloat(prompt('Enter first number: '));
+let number2 = parseFloat(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
