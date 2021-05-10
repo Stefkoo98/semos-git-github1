c8e73d3aeae13009f7afa1d06b81f823aac9b99b
Stefkoo98 <Simjanovski98@outlook.com>
2021-05-10 22:03:17 +0200

    loops

---
+++ JavaScript/lecture4/index.js
@@ -0,0 +1,75 @@
+// 5! = 1*2*3*4*5; - faktoriel!
+
+// let broj = Number(prompt("Vnesi broj"));
+// let rezultat = 1;
+// let i = 1;
+
+// while(i <= broj){
+//     rezultat*=i;
+//     i++;
+// }
+// console.log(rezultat);
+
+
+// let broevi = ["nula","eden","dva","tri","cetiri","pet","sest","sedum","osum","devet"];
+// let i = 0;
+
+// while(i < broevi.length){
+//     console.log(broevi[i]);
+//     i++;
+// }
+
+
+// do while  loop// 
+
+// do{
+//    naredbi; 
+// }
+// while(uslov); // sintaksa za " do while"
+
+
+// let i = 1;
+// let zbir = 0;
+
+// do{
+//     zbir+=i;
+//     i++;
+// }
+// while(i>10);
+// console.log(zbir);
+
+
+// for loop // 
+
+// for(inicijalizacija(let i = 0); uslov(i<10); inkrement/dekrement(i++,i--,i+=10)){
+//     naredbi;
+// }
+
+
+// for(let i = 0;i<=10;i++){
+//     console.log(i);
+// }
+
+
+// let zbir = 0;
+// for(let i = 1; i<=10; i++){
+//     zbir+=i;
+// }
+// console.log(zbir);
+
+
+// let x = 2;
+// let n = 3;
+// let y = 1;
+
+// for(i = 0; i<n; i++){
+//     y=y*x;
+// }
+// console.log(y);
+
+
+let niza = [1,2,3,4,6,20,30];
+
+for(i = 0; i<niza.length; i++){
+    console.log(niza[i]);
+}
