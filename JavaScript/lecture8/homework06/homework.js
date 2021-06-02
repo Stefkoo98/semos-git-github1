7bed50e71a51abad0440b1b4844eca4ded9530e5
Stefkoo98 <Simjanovski98@outlook.com>
2021-06-02 19:15:34 +0200

    Dom-document-object-model

--- JavaScript/lecture8/homework06/homework.js
+++ JavaScript/lecture8/homework06/homework.js
@@ -1,23 +1,23 @@
 // 1. Да се напише функција за печатење на листа во html со користење на низа како параметар и истата да се искористи за креирање на неколку листи.
 
-let names = ['Stefan', 'Darko', 'Bojan', 'Zoran'];
-let test = ['test1', 'test2', 'test3'];
+// let names = ['Stefan', 'Darko', 'Bojan', 'Zoran'];
+// let test = ['test1', 'test2', 'test3'];
 
 
-let createLists = (niza) => {
-  let unorderedList = document.createElement('ul');
+// let createLists = (niza) => {
+//   let unorderedList = document.createElement('ul');
 
-  for (key of niza) {
-    let listElement = document.createElement('li');
-    listElement.innerText = key;
-    unorderedList.appendChild(listElement);
-  }
+//   for (key of niza) {
+//     let listElement = document.createElement('li');
+//     listElement.innerText = key;
+//     unorderedList.appendChild(listElement);
+//   }
 
-  document.body.appendChild(unorderedList);
-}
+//   document.body.appendChild(unorderedList);
+// }
 
-createLists(names);
-createLists(test);
+// createLists(names);
+// createLists(test);
 
 // 2.Да се креира input елемент и копче во html и да се внесе вашето име или некој текст во Input полето. При кликање на копчето со alert да се прикаже она што е внесено.
 
