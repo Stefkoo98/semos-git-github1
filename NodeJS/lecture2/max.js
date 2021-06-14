394c30ffa9d8c382606ff03ab0f7d0e2431634cc
Stefkoo98 <Simjanovski98@outlook.com>
2021-06-14 22:02:52 +0200

    nodeJs

--- NodeJS/lecture2/max.js
+++ NodeJS/lecture2/max.js
@@ -1,7 +1,7 @@
 let numbers = [1, 4, 3, 5, 4, 3];
 // Odredi go najgolemiot broj vo nizata
 
-let max = Math.min();
+let max = Math.min(1, 4, 3, 5, 4, 3);
 numbers.forEach(currentNumber => {
     if(currentNumber > max) {
         max = currentNumber;
--- NodeJS/lecture2/min.js
+++ NodeJS/lecture2/min.js
@@ -2,12 +2,10 @@ let numbers = [1, 4, 3, 5, 4, 3];
 // Odredi go najgolemiot broj vo nizata
 
 let min = Math.max(1, 4, 3, 5, 4, 3)
-
-let max = Math.min(1, 4, 3, 5, 4, 3);
 numbers.forEach(currentNumber => {
     if(currentNumber > max) {
         max = currentNumber;
     }
 });
 
-console.log(max);
+console.log(min);
