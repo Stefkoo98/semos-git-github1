94aab4829574aff7bfcaf3a1124eaaa1d2dc17a9
Stefkoo98 <Simjanovski98@outlook.com>
2021-06-16 19:39:08 +0200

    async cb-callbacks func

--- NodeJS/lecture3/app.js
+++ NodeJS/lecture3/app.js
@@ -1,3 +1,21 @@
-let arsenalPlayers = require('./players')
+const players = require('./players');
 
-arsenalPlayers(arsenalPlayers);
+players.forEach(player => {
+  if (player.club == 'Arsenal') {
+    player.is_favorite = true;
+  }
+
+  if (player.nationality == 'Macedonia') {
+    const parts = player.name.split(' ');
+    const name = parts[0];
+    const lastName = parts[1];
+
+    console.log('=================');
+    console.log('*****************');
+    console.log(`${name} ${lastName}: ${player.club} | Age: ${player.age}`);
+    console.log('*****************');
+    console.log('=================');
+  }
+});
+
+console.log(players);
---
+++ NodeJS/lecture3/players.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+      name: 'Pierre Aubameyang',
+      club: 'Arsenal',
+      nationality: 'Gabon',
+      age: 32
+    },
+    {
+      name: 'Leo Messi',
+      club: 'Barcelona',
+      nationality: 'Argentina',
+      age: 35
+    },
+    {
+      name: 'Goran Pandev',
+      club: 'Genoa',
+      nationality: 'Macedonia',
+      age: 38
+    },
+    {
+      name: 'Kieran Tierney',
+      club: 'Arsenal',
+      nationality: 'Scotland',
+      age: 24
+    },
+    {
+      name: 'Enis Bardhi',
+      club: 'Levante',
+      nationality: 'Macedonia',
+      age: 25
+    }
+  ]
