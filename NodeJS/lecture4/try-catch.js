92cdb7cd90a4ac4156e810618617db894a588e30
Stefkoo98 <81189653+Stefkoo98@users.noreply.github.com>
2021-06-20 16:16:23 +0200

    Delete try-catch.js

--- NodeJS/lecture4/try-catch.js
+++
@@ -1,9 +0,0 @@
-try {
-    let a = 2 + 3;
-    // .. code code code
-    if (a < 10) {
-      throw new Error('Provizijata e pogresno presmetana');
-    }
-  } catch (error) {
-    console.log(`Prikazi na display: ${error}`)
-  }
