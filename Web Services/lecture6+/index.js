ac2b3ac86be3624e4fd00a650d38e987e6628f2f
Stefkoo98 <Simjanovski98@outlook.com>
2021-11-04 13:54:32 +0100

    multitenancy + file upload service

---
+++ Web Services/lecture6+/index.js
@@ -0,0 +1,33 @@
+require('./pkg/db');
+const express = require('express');
+const jwt = require('express-jwt');
+const handlers = require('./handlers/auth')
+
+const api = express();
+
+api.use(express.json());
+api.use(jwt({
+    secret: 'secretpassword',
+    algorithms: ['HS256']
+}).unless({ // you don't need jwt for these routes
+    path: [
+        '/auth/login',
+        '/auth/create-account',
+    ]
+}
+)
+);
+
+api.post('/auth/login', handlers.login);
+api.get('/auth/validate', handlers.validate);
+api.get('/auth/renew-jwt', handlers.renew);
+api.post('/auth/create-account', handlers.createAccount);
+api.post('/auth/forot-password', handlers.forgotPassword);
+api.post('/auth/reset-password', handlers.resetPassword);
+
+api.listen(4000, err => {
+    if (err) {
+        return console.log('Could not start server', err);
+    }
+    console.log('Server successfully started on port 4000');
+})
