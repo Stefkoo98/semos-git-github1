feb5af9fea93f0e46f314dbbd780aaf91e3775d6
Stefkoo98 <Simjanovski98@outlook.com>
2021-12-01 15:29:56 +0100

    popUp pics

---
+++ ReactJS/lecture9/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from 'react-router-dom';
+import { Albums } from "./components/Albums";
+import { Posts } from "./components/Posts";
+import { Nav } from './components/Nav';
+import { Gallery } from "./components/Gallery";
+import { API_URL } from './components/constants';
+
+
+export function App() {
+
+  const [posts, setPosts] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [selectedPhoto, setSelectedPhoto] = useState({});
+
+  useEffect(() => {
+    //posts
+    fetch(`${API_URL}/posts`)
+      .then(res => res.json())
+      .then(json => setPosts(json))
+      .catch(err => {
+        alert(err);
+      })
+    //photos
+    fetch(`${API_URL}/photos`)
+      .then(res => res.json())
+      .then(json => setPhotos(json))
+      .catch(err => { alert(err) })
+  }, [])
+
+  function openPhoto(photo) {
+    setSelectedPhoto(photo)
+  }
+  function closePhoto() {
+    setSelectedPhoto({});
+  }
+
+  function deletePhoto() {
+    setPhotos([...photos.filter(item => item.id !== selectedPhoto.id)]);
+    setSelectedPhoto({});
+  }
+
+  return (
+    <div id="app">
+      <Nav />
+      <Switch>
+        <Route path="/albums" component={Albums} />
+        <Route path="/posts" render={() => {
+          return <Posts listOfPosts={posts} />
+        }} />
+        <Route path="/gallery" render={() => {
+          return <Gallery photoList={photos}
+            openPhoto={openPhoto}
+            selectedImage={selectedPhoto.url}
+            closePhoto={closePhoto}
+            deletePhoto={deletePhoto} />
+        }} />
+      </Switch>
+    </div>
+  )
+}
